e5037064efec30b18540353152bc780a566e5be9
supriya-1997 <67093290+supriya-1997@users.noreply.github.com>
2020-07-30 19:26:56 +0530

    react app

--- src/Amazon/AmazonFooter.js
+++ src/Amazon/AmazonFooter.js
@@ -1,62 +1,56 @@
 import React from 'react'
 
-const AmazonFooter=()=> {
+const AmazonFooter = () => {
     return (
         <>
-        <div style={{backgroundColor:"#A9A9A9",textAlign:"center"}}>Back to top </div>
-       
-        <div className="row bg-dark ">
-            <div className="row mx-auto">
-                <div className="col-2 text-white p-2 mt-2 ">
-                    <h6>Get to Know Us</h6>
-                    <a href="#" className="text-white">Careers</a><br/>
-                    <a href="#" className="text-white">Blog</a><br />
-                    <a href="#" className="text-white">About Amazon</a><br />
-                    <a href="#" className="text-white">Investor Relations</a><br />
-                    <a href="#" className="text-white">Amazon Devices</a><br/>
-                    <a href="#" className="text-white">Amazon Tours</a>
-                </div>
-                <div className="col-3 text-white p-2 mt-2">
-                    <h6>Make Money With us</h6>
-                    <a href="#" className="text-white">Sell on Amazon</a><br />
-                    <a href="#" className="text-white">Sell Your Services on Amazon</a><br />
-                    <a href="#" className="text-white">Sell on Amazon Business</a><br />
-                    <a href="#" className="text-white" >Sell Your Apps on Amazon</a><br />
-                   <a href="#" className="text-white">Report infringement</a><br />
-                    <a href="#" className="text-white">Become an Affiliate</a><br />
-                    <a href="#" className="text-white">Advertise Your Products</a><br />
-                    <a href="#" className="text-white">Self-Publish with Us</a>
-                </div>
-                <div className="col-4 text-white p-2 mt-1">
-                    <h6>Amazon Payment Products</h6>
-                    <a href="#" className="text-white">Amazon Business Card</a><br />
-                    <a href="#" className="text-white">Shop with Points</a><br />
-                    <a href="#" className="text-white">Reload Your Balance</a><br />
-                    <a href="#" className="text-white">Amazon Currency Converter</a><br />
-                    
-                </div>
-                <div className="col-3 text-white p-2 mt-2">
-                    <h6>Let Us Help You</h6>
-                    <a href="#" className="text-white">Amazon and COVID-19</a><br />
-                    <a href="#" className="text-white">Your Account</a><br />
-                    <a href="#" className="text-white">Your Orders</a><br />
-                    <a href="#" className="text-white">Shipping Rates & Policies</a><br />
-                    <a href="#" className="text-white">Returns & Replacements</a><br />
-                    <a href="#" className="text-white">Manage Your Content and Devices</a><br />
-                    <a href="#" className="text-white">Amazon Assistant</a><br />
-                    <a href="#" className="text-light">Help</a>
-                </div>
-                
-                
 
+            <div style={{ backgroundColor: "#A9A9A9", textAlign: "center", width: "100%" }}>Back to top  </div>
+            <div className="container-fluid">
+                <div className="row bg-dark">
+                    <div className="col-md-4 col-lg-3 text-white">
+                        <h6>Get to Know Us</h6>
+                        <div href="#" className="text-white">Careers</div>
+                        <div href="#" className="text-white">Blog</div>
+                        <div href="#" className="text-white">About Amazon</div>
+                        <div href="#" className="text-white">Investor Relations</div>
+                        <div href="#" className="text-white">Amazon Devices</div>
+                        <div href="#" className="text-white">Amazon Tours</div>
+                    </div>
+                    <div className="col-md-4 col-lg-3 text-white">
+                        <h6>Make Money With us</h6>
+                        <div href="#" className="text-white">Sell on Amazon</div>
+                        <div href="#" className="text-white">Sell Your Services on Amazon</div>
+                        <div href="#" className="text-white">Sell on Amazon Business</div>
+                        <div href="#" className="text-white" >Sell Your Apps on Amazon</div>
+                        <div href="#" className="text-white">Report infringement</div>
+                        <div href="#" className="text-white">Become an Affiliate</div>
+                        <div href="#" className="text-white">Advertise Your Products</div>
+                        <div href="#" className="text-white">Self-Publish with Us</div>
+                    </div>
+                    <div className="col-md-4 col-lg-3 text-white">
+                        <h6>Amazon Payment Products</h6>
+                        <div href="#" className="text-white">Amazon Business Card</div>
+                        <div href="#" className="text-white">Shop with Points</div>
+                        <div href="#" className="text-white">Reload Your Balance</div>
+                        <div href="#" className="text-white">Amazon Currency Converter</div>
+
+                    </div>
+                    <div className="col-md-4 col-lg-3 text-white">
+                        <h6>Let Us Help You</h6>
+                        <div href="#" className="text-white">Amazon and COVID-19</div>
+                        <div href="#" className="text-white">Your Account</div>
+                        <div href="#" className="text-white">Your Orders</div>
+                        <div href="#" className="text-white">Shipping Rates & Policies</div>
+                        <div href="#" className="text-white">Returns & Replacements</div>
+                        <div href="#" className="text-white">Manage Your Content and Devices</div>
+                        <div href="#" className="text-white">Amazon Assistant</div>
+                        <div href="#" className="text-light">Help</div>
+                    </div>
+                </div>
             </div>
-            </div>
-            
-</>
 
-
-
+        </>
     )
 }
 
-export default AmazonFooter
+export default AmazonFooter;
